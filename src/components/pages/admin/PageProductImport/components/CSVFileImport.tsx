3e34e7b83de4a8d67d1dcc6f091d347e5f5ccdc6
Gökhan Ünal <unal_gokhan@outlook.com>
2024-11-11 21:53:44 +0300

    wip task 7

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 type CSVFileImportProps = {
   url: string;
@@ -23,32 +23,37 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   };
 
   const uploadFile = async () => {
-    console.log("uploadFile to", url);
-
-    // Get the presigned URL
-    // const response = await axios({
-    //   method: "GET",
-    //   url,
-    //   params: {
-    //     name: encodeURIComponent(file.name),
-    //   },
-    // });
-    // console.log("File to upload: ", file.name);
-    // console.log("Uploading to: ", response.data);
-    // const result = await fetch(response.data, {
-    //   method: "PUT",
-    //   body: file,
-    // });
-    // console.log("Result: ", result);
-    // setFile("");
+    console.log('uploadFile to', url);
+    try {
+      const authToken = localStorage.getItem('authorization_token');
+      const response = await axios({
+        method: 'GET',
+        url,
+        headers: {
+          Authorization: `${authToken}`,
+        },
+        params: {
+          name: encodeURIComponent(file?.name || ''),
+        },
+      });
+      const result = await fetch(
+        encodeURI(`${response.data}?name=${file?.name}`),
+        {
+          method: 'GET',
+        }
+      );
+      console.log('Result: ', result);
+    } catch (err) {
+      console.log('err message', err.message);
+    }
   };
   return (
     <Box>
-      <Typography variant="h6" gutterBottom>
+      <Typography variant='h6' gutterBottom>
         {title}
       </Typography>
       {!file ? (
-        <input type="file" onChange={onFileChange} />
+        <input type='file' onChange={onFileChange} />
       ) : (
         <div>
           <button onClick={removeFile}>Remove file</button>
